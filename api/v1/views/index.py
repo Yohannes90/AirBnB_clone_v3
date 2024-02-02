daf8ac8aad3954865fa4629e207e2f682a8cb507
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-02-02 18:50:24 +0300

    updating api/v1/views/index.py to Create an endpoint that retrieves the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """Initialize Blueprint views"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -9,6 +10,19 @@ def hbnbStatus():
     """hbnb status"""
     response = {"status": "OK"}
     return jsonify(response)
+
+@app_views.route('/stats', methods=['GET'])
+def hbnbStats():
+    """hbnb stats retrieves the number of each objects by type"""
+    stats = {
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User')
+    }
+    return jsonify(stats)
 
 
 if __name__ == "__main__":
