6e18fdb069e856f4c7980e6f028546ce0efe5d32
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-02-02 20:33:53 +0300

    adding states.py in views and updating index.py to import it

---
+++ api/v1/views/states.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""Initialize Blueprint views"""
+from api.v1.views import app_views
+from flask import abort, request, jsonify
+from models import storage
+from models.state import State
+
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def getAllStates():
+    """retrives list of all state objects"""
+    states = storage.all(State).value()
+    state_list = [state.to_dict() for state in states]
+    return jsonify(state_list)
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def getState(state_id):
+    """retrives specific state object with its id"""
+    state = storage.get(State, state_id)
+    if state:
+        return jsonify(state.to_dict())
+    abort(404)
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def deleteState(state_id):
+    """deletes a State object"""
+    state = storage.get(State, state_id)
+    if state:
+        storage.delete(state)
+        storage.save()
+        return jsonify({}), 200
+    abort(404)
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def createState():
+    """creates a State object"""
+    kwargs = request.get_json()
+    if kwargs:
+        if 'name' in kwargs:
+            state = State(**kwargs)
+            state.save()
+            return jsonify(state.to_dict()), 201
+        abort(400, 'Missing name')
+    abort(404, 'Not a JSON')
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def updateState(state_id):
+    """update a State object"""
+    state = storage.get(State, state_id)
+    if state:
+        kwargs = request.get_json()
+        if kwargs:
+            ignore_keys = ['id', 'created_at', 'updated_at']
+            for key, value in kwargs.items():
+                if key not in ignore_keys:
+                    setattr(state, key, value)
+            state.save()
+            return jsonify(state.to_dict()), 200
+        abort(404, 'Not a JSON')
+    abort(404)
