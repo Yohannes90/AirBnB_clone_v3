6e2fa71336e87ebba6627ae76b5fe3fb155c5058
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-02-03 21:45:03 +0300

    updating index.py for pycodestyle correction

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,6 +11,7 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def hbnbStatus():
     """hbnb status"""
