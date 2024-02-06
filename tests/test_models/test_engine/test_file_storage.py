bf21b1601338d54de4cfc3fa5c99b720b7214d1a
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-02-06 13:08:16 +0300

    updating test_file_storage and test_db_storage for correction to question 1

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -119,11 +119,11 @@ class TestFileStorage(unittest.TestCase):
     def test_get(self):
         """Test get method properly retrives objects"""
         storage = FileStorage()
-        self.assertIs(storage.get("User", "usr"), None)
+        self.assertIs(storage.get(User, "usr"), None)
         self.assertIs(storage.get("usr", "usr"), None)
         new_user = User()
         new_user.save()
-        self.assertIs(storage.get("User", new_user.id), new_user)
+        self.assertIs(storage.get(User, new_user.id), new_user)
 
     @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db',
                      "not testing file storage")
@@ -132,9 +132,9 @@ class TestFileStorage(unittest.TestCase):
         storage = FileStorage()
         initial_count = len(storage.all())
         self.assertEqual(storage.count(), initial_count)
-        state_count = len(storage.all("State"))
-        self.assertEqual(storage.count("State"), state_count)
+        state_count = len(storage.all(State))
+        self.assertEqual(storage.count(State), state_count)
         new_state = State()
         new_state.save()
-        self.assertEqual(storage.count("State"), state_count + 1)
+        self.assertEqual(storage.count(State), state_count + 1)
         self.assertEqual(storage.count(), initial_count + 1)
