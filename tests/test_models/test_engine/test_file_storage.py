6f3383539676d10e56de9963bf39c30ff6aa9073
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-02-06 17:58:28 +0300

    updating test_file_storage for correction of errors in test get

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -124,7 +124,7 @@ class TestFileStorage(unittest.TestCase):
         self.assertIs(storage.get("usr", "usr"), None)
         new_user = User()
         new_user.save()
-        self.assertIs(storage.get(User, new_user.id), new_user)
+        self.assertIs(storage.get("User", new_user.id), new_user)
 
     @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db',
                      "not testing file storage")
